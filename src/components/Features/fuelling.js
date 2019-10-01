7927ae6f9200735f3020f4992d4d0007dd5ef2d7
Lucas Bastos <lucas.bastos@interakt.com.br>
2019-10-01 16:35:04 -0300

    Invert features order on mobile

--- src/components/Features/fuelling.js
+++ src/components/Features/fuelling.js
@@ -1,68 +1,71 @@
 import React from "react";
 import Img from "gatsby-image";
+import { Container, Row, Col } from "react-grid-system";
 import { FaCheck } from "react-icons/fa";
 
 export default function fuelling({ fuellingFeature }) {
   return (
     <div className="feature">
       <div style={{ paddingTop: "6rem" }} id="fuelling-link"></div>
-      <div className="columns is-multiline is-variable is-6">
-        <div className="column is-half is-mobile">
-          <div className="mobile-container">
-            <div className="feature-title">
-              <h3 className="is-size-3">Gestão de Abastecimento</h3>
-              <h4 className="is-size-5">
-                Tenha um maior controle da sua operação
-              </h4>
+      <Container>
+        <Row>
+          <Col lg={6} push={{ lg: 6 }}>
+            <div className="mobile-container">
+              <div className="has-text-centered">
+                <Img fluid={fuellingFeature} alt="Abastecimento" />
+              </div>
             </div>
-            <div className="feature-description">
-              <p>
-                Realize o controle de abastecimentos dos seus veículos e
-                equipamentos para gerir os seus gastos com combustível
-              </p>
-            </div>
-            <div className="feature-items">
-              <div className="columns is-mobile is-1 is-variable">
-                <div className="column is-half">
-                  <ul className="feature-list">
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Lançamento de abastecimentos pelo App
-                    </li>
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Registro de abastecimento
-                    </li>
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Cálculo de autonomia
-                    </li>
-                  </ul>
-                </div>
-                <div className="column is-half">
-                  <ul className="feature-list">
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Dashboard interativo
-                    </li>
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Controle de estoque para postos internos
-                    </li>
-                  </ul>
+          </Col>
+          <Col lg={6} pull={{ lg: 6 }}>
+            <div className="mobile-container">
+              <div className="feature-title">
+                <h3 className="is-size-3">Gestão de Abastecimento</h3>
+                <h4 className="is-size-5">
+                  Tenha um maior controle da sua operação
+                </h4>
+              </div>
+              <div className="feature-description">
+                <p>
+                  Realize o controle de abastecimentos dos seus veículos e
+                  equipamentos para gerir os seus gastos com combustível
+                </p>
+              </div>
+              <div className="feature-items">
+                <div className="columns is-mobile is-1 is-variable">
+                  <div className="column is-half">
+                    <ul className="feature-list">
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Lançamento de abastecimentos pelo App
+                      </li>
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Registro de abastecimento
+                      </li>
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Cálculo de autonomia
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="column is-half">
+                    <ul className="feature-list">
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Dashboard interativo
+                      </li>
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Controle de estoque para postos internos
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="column is-half">
-          <div className="mobile-container">
-            <div className="has-text-centered">
-              <Img fluid={fuellingFeature} alt="Abastecimento" />
-            </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/Features/monitoring.js
+++ src/components/Features/monitoring.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col } from "react-grid-system";
 import Img from "gatsby-image";
 import { FaCheck } from "react-icons/fa";
 
@@ -6,64 +7,66 @@ export default function monitoring({ monitorFeature }) {
   return (
     <div className="feature">
       <div style={{ paddingTop: "4rem" }} id="monitor-link"></div>
-      <div className="columns is-multiline is-variable is-6">
-        <div className="column is-half">
-          <div className="mobile-container">
-            <div style={{ height: "2rem" }}></div>
-            <div className="feature-title">
-              <h3 className="is-size-3">Monitoramento</h3>
-              <h4 className="is-size-5">
-                Tenha um maior controle da sua operação
-              </h4>
+      <Container>
+        <Row>
+          <Col lg={6} push={{ lg: 6 }}>
+            <div className="mobile-container">
+              <div className="has-text-centered">
+                <Img fluid={monitorFeature} alt="Rastreamento" />
+              </div>
             </div>
-            <div className="feature-description">
-              <p>
-                Acompanhe a localização da sua frota em tempo real e consulte
-                relatórios de eficiência do seu negócio
-              </p>
-            </div>
-            <div className="feature-items">
-              <div className="columns is-mobile is-variable is-0">
-                <div className="column is-half">
-                  <ul className="feature-list">
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Limite de velocidade
-                    </li>
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Pontos de parada
-                    </li>
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Horas ligado + Horas parado
-                    </li>
-                  </ul>
-                </div>
-                <div className="column is-half">
-                  <ul className="feature-list">
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Cercas virtuais
-                    </li>
-                    <li>
-                      <FaCheck className="feature-icon" />
-                      Alertas de manutenção
-                    </li>
-                  </ul>
+          </Col>
+          <Col lg={6} pull={{ lg: 6 }}>
+            <div className="mobile-container">
+              <div style={{ height: "2rem" }}></div>
+              <div className="feature-title">
+                <h3 className="is-size-3">Monitoramento</h3>
+                <h4 className="is-size-5">
+                  Tenha um maior controle da sua operação
+                </h4>
+              </div>
+              <div className="feature-description">
+                <p>
+                  Acompanhe a localização da sua frota em tempo real e consulte
+                  relatórios de eficiência do seu negócio
+                </p>
+              </div>
+              <div className="feature-items">
+                <div className="columns is-mobile is-variable is-0">
+                  <div className="column is-half">
+                    <ul className="feature-list">
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Limite de velocidade
+                      </li>
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Pontos de parada
+                      </li>
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Horas ligado + Horas parado
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="column is-half">
+                    <ul className="feature-list">
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Cercas virtuais
+                      </li>
+                      <li>
+                        <FaCheck className="feature-icon" />
+                        Alertas de manutenção
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="column is-half">
-          <div className="mobile-container">
-            <div className="has-text-centered">
-              <Img fluid={monitorFeature} alt="Rastreamento" />
-            </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
