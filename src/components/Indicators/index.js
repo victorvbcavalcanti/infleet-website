a04fab9182116cd001ab530f122cf2a3fb01a3de
Lucas Bastos <lucas.bastos@interakt.com.br>
2019-10-01 14:58:25 -0300

    Adjust indicators size

--- src/components/Indicators/index.js
+++ src/components/Indicators/index.js
@@ -1,30 +1,39 @@
 import React from "react";
+import { Container, Row, Col } from "react-grid-system";
 
 export default function index() {
   return (
     <div className="indicators-section">
-      <div className="container">
-        <section className="level">
-          <div className="level-item has-text-centered">
-            <div>
-              <p className="is-size-2">+ 10.000.000</p>
-              <p className="heading">km monitorados</p>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <div className="has-text-centered indicator-item">
+              <div>
+                <p style={{ fontSize: "3.5rem", paddingTop: '1rem' }} className="indicator-number">
+                  + 10.000.000
+                </p>
+                <p className="heading">km monitorados</p>
+              </div>
             </div>
-          </div>
-          <div className="level-item has-text-centered">
-            <div>
-              <p className="is-size-2">+ 10</p>
-              <p className="heading">prêmios de inovação</p>
+          </Col>
+          <Col md={4}>
+            <div className="has-text-centered indicator-item">
+              <div>
+                <p className="indicator-number">+ 10</p>
+                <p className="heading">prêmios de inovação</p>
+              </div>
             </div>
-          </div>
-          <div className="level-item has-text-centered">
-            <div>
-              <p className="is-size-2">100%</p>
-              <p className="heading">território nacional</p>
+          </Col>
+          <Col md={4}>
+            <div className="has-text-centered indicator-item">
+              <div>
+                <p className="indicator-number">100%</p>
+                <p className="heading">território nacional</p>
+              </div>
             </div>
-          </div>
-        </section>
-      </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
